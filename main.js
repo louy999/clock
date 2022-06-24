695f74691eb00fc2ad65779354ac96bd74df2940
louy15 <louyhany999@gmail.com>
2022-06-24 08:24:04 +0200

    project oclock

---
+++ main.js
@@ -0,0 +1,26 @@
+let secT = 0;
+let minT = 0;
+let hourT = 0;
+i = 0;
+
+// setInterval(function sec() {
+//   secT = secT + 6;
+//   let sec = document.querySelector(".sec");
+//   sec.style.transform = `rotate(${secT}deg)`;
+//   i++;
+//   console.log(i);
+// }, 1000);
+// setInterval(function min() {
+//   minT = minT + ;
+//   let sec = document.querySelector(".min");
+//   sec.style.transform = `rotate(${minT}deg)`;
+//   i++;
+//   console.log(i);
+// }, 100);
+// setInterval(function sec() {
+//   hourT = hourT + 0.001;
+//   let sec = document.querySelector(".hour");
+//   sec.style.transform = `rotate(${hourT}deg)`;
+//   i++;
+//   console.log(i);
+// }, 1000);
