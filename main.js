0a53505c791af5bf6fb43bc4d229921fe87dffd6
louy999 <louyhany999@gmail.com>
2022-06-26 14:11:44 +0200

    fix oclock

--- main.js
+++ main.js
@@ -3,24 +3,24 @@ let minT = 0;
 let hourT = 0;
 i = 0;
 
-// setInterval(function sec() {
-//   secT = secT + 6;
-//   let sec = document.querySelector(".sec");
-//   sec.style.transform = `rotate(${secT}deg)`;
-//   i++;
-//   console.log(i);
-// }, 1000);
-// setInterval(function min() {
-//   minT = minT + ;
-//   let sec = document.querySelector(".min");
-//   sec.style.transform = `rotate(${minT}deg)`;
-//   i++;
-//   console.log(i);
-// }, 100);
-// setInterval(function sec() {
-//   hourT = hourT + 0.001;
-//   let sec = document.querySelector(".hour");
-//   sec.style.transform = `rotate(${hourT}deg)`;
-//   i++;
-//   console.log(i);
-// }, 1000);
+setInterval(function sec() {
+  secT = secT + 6;
+  let sec = document.querySelector(".sec");
+  sec.style.transform = `rotate(${secT}deg)`;
+  i++;
+  console.log(i);
+}, 1000);
+setInterval(function min() {
+  minT = minT + 6 / 60;
+  let sec = document.querySelector(".min");
+  sec.style.transform = `rotate(${minT}deg)`;
+  i++;
+  console.log(i);
+}, 1000);
+setInterval(function sec() {
+  hourT = hourT + 6 / 60 / 12;
+  let sec = document.querySelector(".hour");
+  sec.style.transform = `rotate(${hourT}deg)`;
+  i++;
+  console.log(i);
+}, 1000);
